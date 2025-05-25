0f2e06c835cb33425281902d15a85fa31c8d0ad0
John <johnedokpolor@gmail.com>
2025-05-25 12:30:32 +0100

    Testing

--- frontend/utils/axiosInstance.ts
+++ frontend/utils/axiosInstance.ts
@@ -2,9 +2,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://scholarship-xxy2.onrender.com", // Replace with your API base URL
-  baseURL: "http://localhost:1000", // Replace with your API base URL
-  timeout: 30000, // 30 seconds timeout
+  baseURL: "https://scholarship-xxy2.onrender.com", // Replace with your API base URL
+  // baseURL: "http://localhost:1000", // Replace with your API base URL.
+  timeout: 20000, // 20 seconds timeout
   headers: {
     "Content-Type": "application/json",
     // Add other default headers here
