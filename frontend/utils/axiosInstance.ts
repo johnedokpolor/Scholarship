3bc6370ef516a1065ad75fdbd9cf13a5d080fa19
John <johnedokpolor@gmail.com>
2025-05-25 17:01:37 +0100

    added payment email confirmation

--- frontend/utils/axiosInstance.ts
+++ frontend/utils/axiosInstance.ts
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://scholarship-xxy2.onrender.com", // Replace with your API base URL
-  baseURL: "http://localhost:1000", // Replace with your API base URL.
+  baseURL: "https://scholarship-xxy2.onrender.com", // Replace with your API base URL
+  // baseURL: "http://localhost:1000", // Replace with your API base URL.
   timeout: 20000, // 20 seconds timeout
   headers: {
     "Content-Type": "application/json",
